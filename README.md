5b41274f923d21d4f9f1f6f2f3d6a59b85fe5667
Brian Carstensen <brian.carstensen@gmail.com>
2015-08-05 16:34:12 -0500

    Based on the DropdownForm module

---
+++ README.md
@@ -0,0 +1,40 @@
+# ModalForm
+
+This renders a pop-up form with a underlay preventing interaction with the page. Currently used as the dropdown part of a dropdown button (coming soon).
+
+## Usage
+
+```jsx
+import React from 'react';
+import ModalForm from 'modal-form';
+
+class Parent extends React.Component {
+  render() {
+    return <span>
+      <button onClick={this.showPopup}>···</button>
+      {this.state.showingPopup
+        ? <ModalForm onSubmit={this.handleFormSubmit} onCancel={this.hidePopup}>
+            <p>Hello there.</p>
+            <p><label><input type="checkbox" autoFocus /> Cool?</label></p>
+            <p><button type="submit">Done</button></p>
+          </ModalForm>
+        : null}
+    </span>;
+  }
+}
+```
+
+### Props
+
+| Prop | Type | |
+|------|------|-|
+| **anchor** | DOM node | The modal will stick to the the bottom of this element. Defaults to the modal's parent element.
+| **required** | Boolean | If true, the modal cannot be dismissed with the escape key or by clicking somewhere else. Defaults to `false`.
+| **onSubmit** | Function | Call this when the form is submitted.
+| **onCancel** | Function | Call this when the user hits escape or clicks away (if the modal isn't _required_)
+
+## To-do
+
+- Allow anchoring to the window, so the modal is positioned relative to the viewport (like a regular dialog).
+
+- Allow setting the **side** prop, so the modal can be attached to side other than the bottom.
---
+++ test.js
@@ -0,0 +1,95 @@
+var test = require('tape');
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+var ModalForm = require('./index');
+var simulant = window.simulant = require('simulant');
+
+test('ModalForm', function(t) {
+  t.test('is exported', function(t) {
+    t.plan(1);
+    t.notEqual(ModalForm, undefined, 'Exported');
+  });
+
+  t.test('can be created', function(t) {
+    var instance = React.createElement(ModalForm);
+    t.plan(1);
+    t.ok(instance, 'Instance created');
+  });
+
+  t.test('can get a bounding rectangle including margin', function(t) {
+    var element = document.createElement('div');
+    element.style.width = '10px';
+    element.style.height = '10px';
+    element.style.margin = '10px';
+    document.body.appendChild(element);
+
+    var rect = ModalForm.prototype.getRect(element);
+    t.plan(2);
+    t.equal(rect.width, 30, 'Correct rect width');
+    t.equal(rect.height, 30, 'Correct rect height');
+  });
+});
+
+test('An instance', function(t) {
+  var container = document.createElement('div');
+  document.body.appendChild(container);
+
+  var anchor = document.createElement('div');
+  anchor.style.width = '100px';
+  anchor.style.height = '100px';
+  anchor.style.position = 'absolute';
+  anchor.style.left = '100px';
+  anchor.style.top = '100px';
+  document.body.appendChild(anchor);
+
+  var submissions = 0;
+  var cancellations = 0;
+
+  var modal = React.render(React.createElement(ModalForm, {
+    anchor: anchor,
+    onSubmit: function() {
+      submissions += 1;
+    },
+    onCancel: function() {
+      cancellations += 1;
+    }
+  }, React.createElement('div', {
+    style: {
+      width: '50px',
+      height: '50px'
+    }
+  })), container);
+
+  t.test('is positioned under its anchor', function(t) {
+    var form = React.findDOMNode(modal.refs.form);
+    var formCenter = form.offsetLeft + (form.offsetWidth / 2);
+    var formTop = form.offsetTop;
+    var anchorCenter = anchor.offsetLeft + (anchor.offsetWidth / 2);
+    var anchorBottom = anchor.offsetTop + anchor.offsetHeight;
+    t.plan(2);
+    t.equal(formCenter, anchorCenter, 'Lines up with its anchor horizontally');
+    t.ok(formTop >= anchorBottom, 'Sits below its anchor');
+  });
+
+  t.test('calls its onSubmit prop on form submission', function(t) {
+    var form = React.findDOMNode(modal.refs.form);
+    TestUtils.Simulate.submit(form);
+    t.plan(1);
+    t.equal(submissions, 1, 'onSubmit called');
+  });
+
+  t.test('calls its onCancel prop on ESC', function(t) {
+    simulant.fire(window, 'keydown', {
+      which: modal.ESC_KEY
+    });
+    t.plan(1);
+    t.equal(cancellations, 1, 'onCancel called');
+  });
+
+  t.test('calls its onCancel prop on clicking the underlay', function(t) {
+    var underlay = React.findDOMNode(modal.refs.underlay);
+    TestUtils.Simulate.click(underlay);
+    t.plan(1);
+    t.equal(cancellations, 2, 'onCancel called');
+  });
+});
