aaa198c0c6835fc23fe3369cc0322af4cc716873
Brian Carstensen <brian.carstensen@gmail.com>
2015-09-08 15:27:33 +0100

    Big refactor, add dialog

---
+++ anchor.js
@@ -0,0 +1,50 @@
+;(function() {
+  'use strict';
+
+  var React;
+  if (typeof require !== 'undefined') {
+    React = require('react');
+  } else if (typeof window !== 'undefined') {
+    React = window.React;
+  }
+
+  function ModalFormAnchor() {
+    React.Component.apply(this, arguments);
+  }
+
+  ModalFormAnchor.prototype = Object.assign(Object.create(React.Component.prototype), {
+    componentDidMount: function() {
+      this.root = document.createElement('div');
+      this.root.classList.add('modal-form-anchor-root');
+      document.body.appendChild(this.root);
+      this.renderInstance()
+    },
+
+    componentWillUnmount: function() {
+      React.unmountComponentAtNode(this.root);
+      this.root.parentElement.removeChild(this.root);
+      this.root = null;
+      this.instance = null;
+    },
+
+    componentDidUpdate: function() {
+      this.renderInstance();
+    },
+
+    renderInstance: function() {
+      this.instance = React.render(this.props.children, this.root);
+    },
+
+    render: function() {
+      return React.createElement('noscript', {
+        className: 'modal-form-anchor'
+      });
+    }
+  });
+
+  if (typeof module !== 'undefined') {
+    module.exports = ModalFormAnchor;
+  } else if (typeof window !== 'undefined') {
+    window.ZUIModalFormAnchor = ModalFormAnchor;
+  }
+}());
---
+++ dialog.js
@@ -0,0 +1,94 @@
+;(function() {
+  'use strict';
+
+  var React;
+  var ModalFormBase;
+  if (typeof require !== 'undefined') {
+    React = require('react');
+    ModalFormBase = require('./base');
+  } else if (typeof window !== 'undefined') {
+    React = window.React;
+    ModalFormBase = window.ZUIModalFormBase;
+  }
+
+  function mustBePercentString(props, propName, componentName) {
+    if (!/\d%/.test(props[propName])) {
+      return new Error('Must be a percent as a string, e.g. "50%".');
+    }
+  }
+
+  var ModalDialog = React.createClass({
+    statics: {
+      alert: function(message, props) {
+        var container = document.createElement('div');
+        document.body.appendChild(container);
+
+        var promise = new Promise(function(resolve, reject) {
+          function handleSubmit() {
+            resolve.apply(null, arguments);
+            if (props && props.onSubmit !== undefined) {
+              props.onSubmit.apply(null, arguments);
+            }
+          }
+
+          function handleCancel() {
+            reject.apply(null, arguments);
+            if (props && props.onCancel !== undefined) {
+              props.onCancel.apply(null, arguments);
+            }
+          }
+
+          React.render(React.createElement(ModalDialog, Object.assign({}, props, {
+            onSubmit: handleSubmit,
+            onCancel: handleCancel
+          }), message), container);
+        });
+
+        promise.catch(Function.prototype).then(function() {
+          React.unmountComponentAtNode(container);
+          container.parentElement.removeChild(container);
+        });
+
+        return promise;
+      }
+    },
+
+    propTypes: Object.assign({}, ModalFormBase.propTypes, {
+      left: mustBePercentString,
+      top: mustBePercentString
+    }),
+
+    getDefaultProps: function() {
+      return Object.assign({}, ModalFormBase.defaultProps, {
+        left: '50%',
+        top: '40%'
+      });
+    },
+
+    render: function() {
+      var positionStyle = {
+        left: this.props.left,
+        top: this.props.top,
+        transform: 'translate(' + [
+          -1 * parseFloat(this.props.left) + '%',
+          -1 * parseFloat(this.props.top) + '%'
+        ].join(',') + ')'
+      };
+
+      var modalProps = Object.assign({
+        'role': 'dialog'
+      }, this.props, {
+        className: ('modal-dialog ' + (this.props.className || '')).trim(),
+        style: Object.assign(positionStyle, this.props.style)
+      });
+
+      return React.createElement(ModalFormBase, modalProps, this.props.children);
+    }
+  });
+
+  if (typeof module !== 'undefined') {
+    module.exports = ModalDialog;
+  } else if (typeof window !== 'undefined') {
+    window.ZUIModalDialog = ModalDialog;
+  }
+}());
