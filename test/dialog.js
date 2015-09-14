b8ab170942b9721fe9009b5b2f793eec40fb0f9e
Brian Carstensen <brian.carstensen@gmail.com>
2015-09-14 16:32:13 -0500

    Add dialog tests

---
+++ test/dialog.js
@@ -0,0 +1,48 @@
+Object.assign || (Object.assign = require('object-assign'));
+window.Promise || (window.Promise = require('es6-promise').Promise);
+var React = require('react');
+var ModalFormDialog = require('../dialog');
+var assert = require('assert');
+var simulant = require('simulant');
+
+describe('ModalFormDialog', function() {
+  it('exports', function() {
+    assert.equal(typeof ModalFormDialog, 'function');
+  });
+
+  describe('.alert', function() {
+    var id;
+    before(function() {
+      id = Math.random().toString(36).split('.')[1];
+
+      ModalFormDialog.alert(React.createElement('button', {
+        id: id,
+        type: 'submit'
+      }));
+    });
+
+    it('mounts a dialog', function() {
+      assert.ok(document.getElementById(id));
+    });
+
+    it('closes on submit', function(done) {
+      var submitButton = document.getElementById(id);
+      simulant.fire(submitButton, 'click');
+      setTimeout(function() {
+        assert.equal(document.getElementById(id), null);
+        done();
+      });
+    });
+
+    it('closes on cancel', function(done) {
+      simulant.fire(window, 'keydown', {
+        which: 27
+      });
+
+      setTimeout(function() {
+        assert.equal(document.getElementById(id), null);
+        done();
+      });
+    });
+  });
+});
