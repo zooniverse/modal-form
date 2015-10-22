12704f30007e829f83b0c408ba56ff5ca4ad2be1
Brian Carstensen <brian.carstensen@gmail.com>
2015-10-22 14:30:21 -0500

    Allow scrolling

--- base.js
+++ base.js
@@ -16,7 +16,7 @@
   var UNDERLAY_STYLE = {
     bottom: 0,
     left: 0,
-    position: 'fixed',
+    position: 'absolute',
     right: 0,
     top: 0
   };
@@ -30,6 +30,10 @@
     this.handleGlobalKeyDown = this.handleGlobalKeyDown.bind(this);
     this.handleGlobalNavigation = this.handleGlobalNavigation.bind(this);
     this.lastKnownLocation = location.href;
+    this.state = {
+      underlayScrollWidth: 0,
+      underlayScrollHeight: 0
+    };
   }
 
   ModalFormBase.locationChangeEvent = 'locationchange';
@@ -57,6 +61,7 @@
       addEventListener('keydown', this.handleGlobalKeyDown);
       addEventListener('hashchange', this.handleGlobalNavigation);
       addEventListener(ModalFormBase.locationChangeEvent, this.handleGlobalNavigation);
+      this.syncUnderlaySize();
     },
 
     componentWillUnmount: function() {
@@ -96,17 +101,37 @@
     },
 
     renderLoose: function() {
+      var underlaySize = {
+        width: Math.max(this.state.underlayScrollWidth, document.documentElement.offsetWidth, innerWidth) + 'px',
+        height: Math.max(this.state.underlayScrollHeight, document.documentElement.offsetHeight, innerHeight) + 'px'
+      };
       return React.createElement.apply(React, ['div', {
         ref: 'underlay',
         className: ('modal-form-underlay ' + (this.props.className || '')).trim(),
-        style: Object.assign({}, UNDERLAY_STYLE, this.props.underlayStyle),
+        style: Object.assign({}, UNDERLAY_STYLE, underlaySize, this.props.underlayStyle),
         onClick: this.handleUnderlayClick.bind(this)
       }].concat(this.getUnderlayChildren()));
     },
 
     renderWithAnchor: function() {
-      var looseRenderResult = ModalFormBase.prototype.renderLoose.apply(this, arguments);
+      var looseRenderResult = this.renderLoose.apply(this, arguments);
       return React.createElement(ModalFormAnchor, null, looseRenderResult);
+    },
+
+    componentDidUpdate: function() {
+      this.syncUnderlaySize();
+    },
+
+    syncUnderlaySize: function() {
+      var underlay = React.findDOMNode(this.refs.underlay);
+      var widthChanged = underlay.scrollWidth !== this.state.underlayScrollWidth;
+      var heightChanged = underlay.scrollHeight !== this.state.underlayScrollHeight;
+      if (widthChanged || heightChanged) {
+        this.setState({
+          underlayScrollWidth: underlay.scrollWidth,
+          underlayScrollHeight: underlay.scrollHeight
+        });
+      }
     },
 
     handleFormSubmit: function(event) {
